0cd66f399e96a517d7c6970c40228f9ba54af5b0
TeddyAster <103246062+TeddyAster@users.noreply.github.com>
2026-07-04 12:44:16 +0800

    Wwise Audio Manager
    Wwise Audio Manager integration into Unity

---
+++ CiGAJam_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MAINGAME_FOOTSTEP = 1542905893U;
+        static const AkUniqueID PLAY_SFX_ACHIEVED = 861084261U;
+        static const AkUniqueID PLAY_SFX_BAD = 1576397959U;
+        static const AkUniqueID PLAY_SFX_CLICK = 1602989894U;
+        static const AkUniqueID PLAY_SFX_ELECTRICSHOCK = 2654846947U;
+        static const AkUniqueID PLAY_SFX_HIT = 1376640657U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID LEVELEND = 2037204996U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MUSICSWITCH
+        {
+            static const AkUniqueID GROUP = 1445037870U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace MUSICSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME_MUSIC = 3891337659U;
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_MAINGAME = 2185250982U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
