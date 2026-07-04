94e7a62eab4e47f96f1cfab2780403f599111e29
Yuting Wu <122715743+Govern2077@users.noreply.github.com>
2026-07-04 23:23:43 +0800

    111
    无领口加入，能随机生成了

--- Assets/Scripts/捏脸/OnUI/RandomizeButton.cs
+++ Assets/Scripts/捏脸/OnUI/RandomizeButton.cs
@@ -1,13 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RandomizeButton : MonoBehaviour
 {
     public CharacterCustomizer2D customizer;
+    public AnswerGenerator answerGenerator;
     public Button randomButton;
+    public int seed = 1234;
 
     void Start()
     {
-        randomButton.onClick.AddListener(() => customizer.RandomizeAll());
+        //randomButton.onClick.AddListener(() => customizer.RandomizeAll());
+        randomButton.onClick.AddListener(() =>
+        {
+            FaceSaveData faceData = answerGenerator.GenerateAnswerWithSeed(seed);
+            string json = JsonUtility.ToJson(faceData);
+            customizer.LoadFromJson(json);
+        });
     }
 }
